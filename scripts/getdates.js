8912c4337b7de3584e84e31d0cbf2c8ec63354a3
Alderesu <kmoran@byupathway.edu>
2025-01-11 22:57:42 +0800

    Assignment Completed

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = ` ${currentYear}  Kyle Aldrich Moran | Philippines`;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
